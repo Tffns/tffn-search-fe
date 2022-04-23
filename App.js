642da9615783fbdc8c5d2c1854c2fb5cb2afb62c
s-pat <sohanpatel96@gmail.com>
2022-04-22 21:14:24 -0400

    added search bar and search state logic

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { useState } from 'react';
+import Search from './src/components/Search';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("Indian")
+  console.debug("app.js term is : ", searchTerm)
   return (
     <View >
-      
+      <Search setSearchTerm={setSearchTerm}/>
+      <StatusBar style="auto" />
     </View>
   );
 }
